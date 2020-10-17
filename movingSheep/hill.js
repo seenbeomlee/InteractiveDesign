2ca0c9eac6096866d53f8a052903c52698bad91f
Seenbeom Lee <mail@seenblee.com>
2020-10-17 19:29:58 +0900

    #module: moving sheep & hills: 1.1
    > hills are not cut

--- movingSheep/hill.js
+++ movingSheep/hill.js
@@ -29,6 +29,16 @@ export class Hill {
 
     let dots = [];
     cur.x += this.speed;
+
+    if (cur.x > -this.gap) {
+      this.points.unshift({
+        x: -(this.gap * 2),
+        y: this.getY()
+      });
+    } else if (cur.x > this.stageWidth + this.gap) {
+      this.points.splice(-1);
+    }
+
     ctx.moveTo(cur.x, cur.y);
 
     let prevCx = cur.x;
