7e1b344cee0f85899564887957d3a9ebf065e77c
Seenbeom Lee <mail@seenblee.com>
2020-10-17 22:57:16 +0900

    #module: moving sheep & hills: 1.6
    > sheeps are moveing on the hill

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+## movingSheep
+
+# drawing curve hill
+
+# each hill has their own speed
+
+# image sprite (sheep.png)
+
+# FPS to move sheep
+
+# quadCurve coordinating
--- movingSheep/sheep.js
+++ movingSheep/sheep.js
@@ -45,7 +45,7 @@ export class Sheep {
 
     ctx.save();
     ctx.translate(this.x, this.y);
-    ctx.fillStyle = '#000000';
+    ctx.rotate(closest.rotation);
     ctx.drawImage(
       this.img,
       this.imgWidth * this.curFrame,
@@ -74,7 +74,7 @@ export class Sheep {
   }
   
   getY2(x, dot) {
-    const total = 200;
+    const total = 200; /* divide curve line to #200 */
     let pt = this.getPointOnQuad(dot.x1, dot.y1, dot.x2, dot.y2, dot.x3, dot.y3, 0);
     let prevX = pt.x;
     for (let i = 1; i < total; i++) {
@@ -94,10 +94,17 @@ export class Sheep {
   }
 
   getPointOnQuad(x1, y1, x2, y2, x3, y3, t) {
+    const tx = this.quadTangent(x1, x2, x3, t);
+    const ty = this.quadTangent(y1, y2, y3, t);
+    const rotation = -Math.atan2(tx, ty) + (90 * Math.PI / 180) /* find horizontal angle with quad-lines atan == vertical + 90 == horizontal */
     return {
       x: this.getQuadValue(x1, x2, x3, t),
       y: this.getQuadValue(y1, y2, y3, t),
+      rotation: rotation,
     };
   }
 
+  quadTangent(a, b, c, t) {
+    return 2 * (1 - t) * (b - a) + 2 * (c - b) * t;
+  }
 }
